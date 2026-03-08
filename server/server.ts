c4bbcaf6a44cbf27112f2103b10018c3ba8e89d9
yash saini <syash6662@gmail.com>
2026-03-08 11:47:13 +0530

    first commit

---
+++ server/server.ts
@@ -0,0 +1,99 @@
+import 'dotenv/config'
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+import connectDB from './configs/db.js';
+import session from 'express-session';
+import MongoStore from 'connect-mongo';
+import AuthRouter from './routes/AuthRoutes.js';
+import ThumbnailRouter from './routes/ThumbnailRoutes.js';
+import UserRouter from './routes/UserRoutes.js';
+
+
+
+declare module 'express-session' {
+  interface SessionData {
+    isLoggedIn: boolean;
+    userId: string
+  }
+}
+
+await connectDB() 
+
+const app = express();
+
+
+app.use(cors({
+    origin: ['http://localhost:5173', 'http://localhost:3000', 'https://thumblify-blue-eight.vercel.app'],
+    credentials: true
+}))
+
+app.use(express.json());
+
+app.set('trust proxy', 1);
+
+// app.use(session({
+//     secret: process.env.SESSION_SECRET as string,
+//     resave: false,
+//     saveUninitialized: false,
+//     cookie: {maxAge: 7 * 24 * 60 * 60 * 1000,
+//       httpOnly : true,
+//       secure: process.env.NODE_ENV === 'production',
+//       sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+//       path : '/'
+//     }, // cookie valid for 7 days
+//     store : MongoStore.create({
+//         mongoUrl: process.env.MONGODB_URI as string,
+//         collectionName: 'sessions'
+//     })
+// }))
+
+// app.use(session({
+//   secret: process.env.SESSION_SECRET as string,
+//   resave: false,
+//   saveUninitialized: false,
+//   proxy: true,
+//   cookie: {
+//     maxAge: 7 * 24 * 60 * 60 * 1000,
+//     httpOnly: true,
+//     secure: true,
+//     sameSite: "none"
+//   },
+//   store: MongoStore.create({
+//     mongoUrl: process.env.MONGODB_URI as string,
+//     collectionName: "sessions"
+//   })
+// }));
+
+app.use(session({
+  secret: process.env.SESSION_SECRET as string,
+  resave: false,
+  saveUninitialized: false,
+  proxy: true,
+  cookie: {
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    httpOnly: true,
+    secure: false,        // 🔥 change this
+    sameSite: "lax"       // 🔥 change this
+  },
+  store: MongoStore.create({
+    mongoUrl: process.env.MONGODB_URI as string,
+    collectionName: "sessions"
+  })
+}));
+
+
+
+app.get('/', (req: Request, res: Response) => {
+res.send('Server is Live!');
+});
+
+app.use('/api/auth', AuthRouter);
+app.use('/api/thumbnail', ThumbnailRouter);
+app.use('/api/user', UserRouter);
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+console.log(`Server is running at http://localhost:${port}`);
+
+});
